0d47177fd3831c92cb37d1d9fb3e20fc3fee7a98
Furkan Mumcu <furkanmumcucs@gmail.com>
2018-03-07 02:11:24 +0300

    added new role

---
+++ role.repairer.js
@@ -0,0 +1,47 @@
+var roleBuilder = require('role.builder');
+
+var roleRepairer = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+        if(creep.memory.building && creep.carry.energy == 0) {
+            creep.memory.building = false;
+            creep.say('🔄 harvest');
+        }
+        if(!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.building = true;
+            creep.say('🚧 build');
+        }
+
+        if(creep.memory.building) {
+            var structure = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                // the second argument for findClosestByPath is an object which takes
+                // a property called filter which can be a function
+                // we use the arrow operator to define it
+                filter: (s) => s.hits < s.hitsMax && s.structureType != STRUCTURE_WALL
+            });
+
+            // if we find one
+            if (structure != undefined) {
+                // try to repair it, if it is out of range
+                if (creep.repair(structure) == ERR_NOT_IN_RANGE) {
+                    // move towards it
+                    creep.moveTo(structure);
+                }
+            }
+            else{
+                //TODO: make the creep upgrader
+                //roleHarvester.run(creep);
+                roleBuilder.run(creep);
+            }
+        }
+        else { // harvest energy
+            var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+        }
+    }
+};
+
+module.exports = roleRepairer;
