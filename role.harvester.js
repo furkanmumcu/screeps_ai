1b88e5d00362bb8386abd0e6348563562bc4e329
Furkan Mumcu <furkanmumcucs@gmail.com>
2018-03-06 20:38:38 +0300

    Initial commit

---
+++ role.harvester.js
@@ -0,0 +1,36 @@
+var roleBuilder = require('role.builder');
+
+var roleHarvester = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+        //find source and harvest
+        if(creep.carry.energy < creep.carryCapacity) {
+            var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+        }
+        else { // transfer  energy to the structures
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_EXTENSION ||
+                        structure.structureType == STRUCTURE_SPAWN ||
+                        structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+                }
+            });
+            if(targets.length > 0) {
+                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            }
+            else{
+                //console.log("hi")
+                roleBuilder.run(creep);
+
+            }
+        }
+    }
+};
+
+module.exports = roleHarvester;
